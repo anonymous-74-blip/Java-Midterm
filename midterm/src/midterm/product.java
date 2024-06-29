5ef351e14301548650f43fd6f419f6cffedf1e50
Kieran-Browniee <69154850+anonymous-74-blip@users.noreply.github.com>
2024-06-29 06:21:09 -0700

    Add files via upload

---
+++ midterm/src/midterm/product.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midterm;
+
+/**
+ *
+ * @author matri
+ */
+public class product {
+        private int id;
+        private String name;
+        private double price;
+        private double cost;
+        private int stockQuantity;
+    public Product(int id, String name, double price, double cost, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.cost = cost;
+        this.stockQuantity = stockQuantity;
+    }
+
+
+    // Getters and setters omitted for brevity
+}
+
